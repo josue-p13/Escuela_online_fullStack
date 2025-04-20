2d5dd7ca878049ee1a6a71a6dbcd2c9d26327197
josue <josue@example.com>
2025-04-20 13:44:43 -0500

    El principio

---
+++ Front_Vue/src/components/Login.vue
@@ -0,0 +1,19 @@
+<script >
+    export default {
+        name : 'Login',
+    }
+</script>
+
+<template>
+<div>
+    <h1>Login</h1>
+    <form>
+    <input type="text" placeholder="Ingresa tu correo">
+    <input type="password" placeholder="Contraseña">
+    </form>
+</div>
+</template>
+
+<style scoped>
+
+</style>
